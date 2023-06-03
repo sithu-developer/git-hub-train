560056b182b6e6aab32549d50fdae487ef5e51a4
Sithu Naing <sithunaing8209@gmail.com>
2023-06-03 22:00:45 +0630

    factsCommit

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 console.log("hello guys");
 
 console.log("second");
+
+// git remote add origin URL    <= first step 
+// git push origin commitName   <= second step
+
+// git branch -D branchName             <= to delete the branch in my laptop
+// git push origin --delete branchName  <= to delete the branch in git-hub
+// git clone URL    <= to male clone of code from git-hub (fast way to download the code)
